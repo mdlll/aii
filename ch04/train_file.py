a7035f9ad704093ff554213ba6b593d6e8028e9a
mda <mdlhy@qq.com>
2020-12-14 15:32:44 +0800

    111

--- ch04/train_file.py
+++ ch04/train_file.py
@@ -11,10 +11,40 @@ from dataset.mnist import load_mnist
 '''
 4.5.2 的机器学习实现
 '''
+def cross_entropy_error(y, t):
+    # 平均交叉熵函数
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    batch_size = y.shape[0] #有几行
+    # return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size # t的数据非hot-one时候使用这个
+    return -np.sum(t * np.log(y + 1e-7)) / batch_size # t的数据是hot-one时候使用这个
 
+np.set_printoptions(threshold=sys.maxsize)
 # 读入数据
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+# (训练图像, 训练标签), (测试图像, 测试标签)
+#(60000, 784) (60000, 10) (10000, 784) (10000, 10)
 
+# 抽取10个数据
+train_size = x_train.shape[0]
+batch_size = 10
+batch_mask = np.random.choice(train_size, batch_size)
+x_batch = x_train[batch_mask] #10张处理好的图
+t_batch = t_train[batch_mask] #10个训练标签
+
+# y=np.array([[0,1,0.1,0,0,0,0,0,0,0],
+#             [0,0,0.2,0.8,0,0,0,0,0,0]])
+# t=np.array([[0,1,0,0,0,0,0,0,0,0],
+#             [0,0,0,1,0,0,0,0,0,0]])#one-hot
+y = np.array([0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0])
+t = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+print(cross_entropy_error(y,t))
+
+
+
+print(x_train.shape,t_train.shape,x_test.shape, t_test.shape)
+'''
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
 iters_num = 10000  # 适当设定循环的次数
@@ -60,3 +90,6 @@ plt.ylim(0, 9.0)
 plt.xlim(0, 11000)
 plt.legend(loc='lower right')
 plt.show()
+
+
+'''
