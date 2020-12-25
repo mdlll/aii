657bc700c7cc80d6f389ceecf6a229ed2748dc49
mda <mdlhy@qq.com>
2020-12-25 15:00:40 +0800

    lambda is what?

--- ch04/gradient_simplenet.py
+++ ch04/gradient_simplenet.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import sys, os
+
 sys.path.append(os.pardir)  # 为了导入父目录中的文件而进行的设定
 import numpy as np
 from common.functions import softmax, cross_entropy_error
@@ -8,7 +9,7 @@ from common.gradient import numerical_gradient
 
 class simpleNet:
     def __init__(self):
-        self.W = np.random.randn(2,3)
+        self.W = np.random.randn(2, 3)
 
     def predict(self, x):
         return np.dot(x, self.W)
@@ -20,6 +21,7 @@ class simpleNet:
 
         return loss
 
+
 x = np.array([0.6, 0.9])
 t = np.array([0, 0, 1])
 
@@ -27,5 +29,15 @@ net = simpleNet()
 
 f = lambda w: net.loss(x, t)
 dW = numerical_gradient(f, net.W)
+
+'''
+f = lambda x:my_test(x)
+等价于
+def f(x):
+	return my_test(x)
+'''
+
+
+print(net.W)
 
 print(dW)
